8100bf84c9e0605af8235d8626d30b026e91f73f
hanif <hanif@thetigerworks.com>
2018-01-20 15:32:28 +0600

    added-all

--- go/main.go
+++ go/main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
 func main() {
-	fmt.Println("Hello World!!!")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
