419db0ee938e081ae860c6cfc763531bb71f8d8b
hanif <hanif@thetigerworks.com>
2018-01-20 22:21:39 +0600

    added-all

---
+++ go/Dockerfile
@@ -0,0 +1,4 @@
+FROM scratch
+EXPOSE 8080
+COPY go /
+CMD ["/go"]
---
+++ go/build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+rm go
+CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o go main.go
+docker build -t a8uhnf/go:1.0 .
+rm go
+docker push a8uhnf/go:1.0
