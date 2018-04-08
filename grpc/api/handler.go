816b1128f1e22d705f6f82939e8a4e371858f5e6
hanif <hanif@thetigerworks.com>
2018-04-09 02:39:17 +0600

    added-all

--- grpc/api/handler.go
+++ grpc/api/handler.go
@@ -4,24 +4,23 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
 
 	"golang.org/x/net/context"
 )
 
 // Server represents the gRPC server
-type Server struct {
-}
+type Server struct{}
+
+var count int32
 
 // SayHello generates response to a Ping request
 func (s *Server) SayHello(ctx context.Context, in *PingMessage) (*PingMessage, error) {
 	env := os.Getenv("HELLO_WORLD")
-	fmt.Println("---------------", env)
-	n, err := strconv.Atoi(env)
-	if err != nil {
-		return nil, err
-	}
-	fmt.Println("---------------", n)
+
+	fmt.Println("--------------- ENV: ", env)
+	// n, err := strconv.Atoi(env)
+	fmt.Println("--------------- COUNT: ", count)
+	count++
 
 	log.Printf("Receive message %s", in.Greeting)
 	return &PingMessage{Greeting: env}, nil
