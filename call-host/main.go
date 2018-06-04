4146a5b6835f6a1ef115213984bbb51db8008e8a
hanif <abuhanifa@pathao.com>
2018-06-04 11:40:07 +0600

    added-all

--- call-host/main.go
+++ call-host/main.go
@@ -2,24 +2,26 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+
+	"github.com/Sirupsen/logrus"
 )
 
 func main() {
 	fmt.Println("hello World!!!")
-	url := fmt.Sprintf("%s:%s/hello", "http://localhost", "8080") //:8080/hello
-	_, err := http.Get(url)
+	url := fmt.Sprintf("%s:%s/hello", "http://172.17.0.2", "8080") //:8080/hello
+	resp, err := http.Get(url)
 	if err != nil {
-		// panic(err)
 		log.Println("error occured", err)
 	}
-	/* b, err := ioutil.ReadAll(resp.Body)
+	b, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		// panic(err)
-	} */
+		log.Println("error occured....", err)
+	}
 
-	// logrus.Println("-------", string(b))
+	logrus.Println("-------", string(b))
 	for {
 
 	}
