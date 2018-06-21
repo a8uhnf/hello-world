1832101f46f627f69ea3e665ecef1eea0630ddb7
hanif <abuhanifa@pathao.com>
2018-06-21 14:24:18 +0600

    added-all

--- logging-ext/main.go
+++ logging-ext/main.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+
+	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+
+	log.Println(r.Method)
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println(errors.Wrap(fmt.Errorf("test"), "Hello World!!! Testing Error!!!"))
+	log.Println(string(b))
+	log.Println(w.Header())
 }
 
 func main() {
