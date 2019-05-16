75fdba814350f9982b53be291e65f255e9b9af5a
hanif <abuhanifa@pathao.com>
2019-05-16 06:06:02 +0600

    add sidecar-deploy with envoy

--- go/build.sh
+++ go/build.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+if [ -z $1 ]
+then
+    echo "Need to provide image tag..."
+    exit
+fi
+
 rm go
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o go main.go
-docker build -t a8uhnf/go:3.0 .
+docker build -t a8uhnf/go:$1 .
 rm go
-docker push a8uhnf/go:3.0
+docker push a8uhnf/go:$1
--- go/main.go
+++ go/main.go
@@ -10,7 +10,7 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	log.Println("Hello Hnaifa. you dumb ass......")
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "Hi there, I hate %s!", r.URL.Path[1:])
 }
 
 func main() {
