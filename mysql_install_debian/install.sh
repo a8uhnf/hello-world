a3d1c4572102e38aff5171ab331412ddddda9a1e
hanif <abuhanifa@pathao.com>
2021-04-03 15:48:29 +0600

    add edit command to enable connect mysql from any host

--- mysql_install_debian/install.sh
+++ mysql_install_debian/install.sh
@@ -3,3 +3,6 @@ wget https://dev.mysql.com/get/mysql-apt-config_0.8.16-1_all.deb
 dpkg -i mysql-apt-config*
 apt-get update
 apt-get install mysql-community-server
+
+## edit to allow mysql connect from any host, by default it's 127.0.0.1
+# sudo vim /etc/mysql/mysql.conf.d/mysqld.cnf
